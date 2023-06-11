8207f07cd6e860c548e2fe3c1c8c9e168b18b55a
samuelgit <samuelmrf217@gmail.com>
2023-06-10 23:44:56 -0300

    variaveis js

---
+++ src/js/entrar-cadastrar.js
@@ -0,0 +1,16 @@
+// Labels
+const labelNome = document.querySelector("#labelNome")
+const labelEmail = document.querySelector("#labelEmail")
+const labelData = document.querySelector("#labelData")
+const labelSenha = document.querySelector("#labelSenha")
+const labelConfirmarSenha = document.querySelector("#labelConfirmarSenha")
+
+// Inputs
+const InputNome = document.querySelector("#nomeCadastro")
+const InputData = document.querySelector("#dataCadastro")
+const InputEmailCadastro = document.querySelector("#emailCadastro")
+const InputSenhaCadastro = document.querySelector("#senhaCadastro")
+const InputConfirmarSenha = document.querySelector("#confirmarSenhaCadastro")
+
+
+
